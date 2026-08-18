f4c800b47f251522018f43514b954f3e2651fd03
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-18 15:37:52 +0300

    Add book settings persistence contract

--- lib/features/book_library/domain/repositories/book_project_repository.dart
+++ lib/features/book_library/domain/repositories/book_project_repository.dart
@@ -1,4 +1,5 @@
 import 'package:markweft_simple_book/features/book_library/domain/entities/markweft_project.dart';
+import 'package:markweft_template_simple/markweft_template_simple.dart';
 
 abstract interface class BookProjectRepository {
   Future<MarkweftProject?> createProject({required String title});
@@ -14,6 +15,13 @@ abstract interface class BookProjectRepository {
   Future<void> saveMarkdown(
     MarkweftProject project,
     String markdown,
+  );
+
+  Future<BookSettings> loadBookSettings(MarkweftProject project);
+
+  Future<void> saveBookSettings(
+    MarkweftProject project,
+    BookSettings settings,
   );
 
   Future<void> closeProject(MarkweftProject project);
