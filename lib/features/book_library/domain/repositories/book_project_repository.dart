6204a378892f698c9a457134f355829dc1e3e1a8
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-20 14:02:01 +0300

    Keep project repository focused on open/edit operations

--- lib/features/book_library/domain/repositories/book_project_repository.dart
+++ lib/features/book_library/domain/repositories/book_project_repository.dart
@@ -11,8 +11,6 @@ abstract interface class BookProjectRepository {
 
   Future<MarkweftProject> openProject(String projectPath);
 
-  Future<String?> pickAndConvertProjectVersion({required int targetVersion});
-
   Future<List<BookChapterFile>> loadChapters(MarkweftProject project);
 
   Future<BookChapterFile> createChapter(
